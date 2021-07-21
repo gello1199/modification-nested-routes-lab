926f69798dcc31892e6ded372c4e48073b0d0351
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-07-21 18:54:00 -0400

    commit tree 71fb25e70500ef3ea90d9624cc5b9246fa83bc72

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
 class SongsController < ApplicationController
+  # rescue_from ActiveRecord::RecordNotFound, with: :deny_access
+
   def index
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
@@ -25,7 +27,13 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.find_by_id(params[:artist_id])
+      redirect_to artists_path
+    elsif params[:artist_id] && @artist = Artist.find(params[:artist_id])
+      @song = @artist.songs.build
+    else
+      @song = Song.new
+    end
   end
 
   def create
@@ -39,7 +47,14 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id] && !Artist.find_by_id(params[:artist_id])
+      redirect_to artists_path
+    elsif params[:artist_id] && @artist = Artist.find(params[:artist_id])
+      @song = @artist.songs.find_by_id(params[:id])
+      redirect_to artist_songs_path(@artist)
+    else
+      @song = Song.find_by_id(params[:id])
+    end
   end
 
   def update
@@ -64,7 +79,12 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
+
+  # protected
+  # def deny_access
+  #   redirect_to artists_path
+  # end
 end
 
