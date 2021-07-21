c941ff06031116e0e9d8bd24c44f415c45b8a0fa
Angelo Tsaganeas <a.tsaganeas@gmail.com>
2021-07-21 18:54:06 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  # rescue_from ActiveRecord::RecordNotFound, with: :deny_access
+  
+  # protected
+  # def deny_access
+  #   redirect_to artists_path
+  # end
 end
